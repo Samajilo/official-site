627cf79796ef20a9452ac4cfcd4b0ea2fe6fd0c0
leo <leodahal4@gmail.com>
2021-01-22 09:08:16 +0545

    loading spinner done

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,57 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="ma-0">
-        <nuxt />
+      <v-container
+        fill-height
+        fluid
+        class="ma-0"
+      >
+        <div v-if="loading == false">
+          <transition
+            name="fade"
+            mode="out-in"
+          >
+            <nuxt />
+          </transition>
+        </div>
+        <v-row
+          align="center"
+          height="100%"
+          justify="center"
+          v-else
+        >
+          <div class="sk-wave">
+            <div class="sk-wave-rect"></div>
+            <div class="sk-wave-rect"></div>
+            <div class="sk-wave-rect"></div>
+            <div class="sk-wave-rect"></div>
+            <div class="sk-wave-rect"></div>
+          </div>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import 'spinkit/spinkit.min.css'
+
 export default {
+  name: 'indexPage',
   data () {
     return {
+      loading: true
     }
+  },
+  beforeCreate () {
+    setTimeout(() => { this.loading = false }, 2 * 1000)
+    this.$router.beforeEach((to, from, next) => {
+      this.loading = true
+      next()
+    })
+    this.$router.afterEach(() => {
+      setTimeout(() => { this.loading = false }, 0.5 * 1000)
+    })
   }
 }
 </script>
@@ -21,4 +60,15 @@ export default {
     font-family: 'Palatino Linotype', sans-serif;
     font-size: 1.25rem;
   }
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 1s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,15 @@
 <template>
   <div>
-    <div v-if="loading == false">
+    <div>
       <v-main>
-        <transition
-          name="fade"
-          mode="out-in"
-        >
-          <div>
-            <HomePhoto />
-            <DiscoverHistoryLink />
-          </div>
-        </transition>
+        <HomePhoto />
+        <DiscoverHistoryLink />
       </v-main>
     </div>
-    <v-container
-      fill-height
-      fluid
-      v-else
-    >
-      <v-row
-        align="center"
-        height="100vh"
-        justify="center"
-      >
-        <client-only>
-          <spring-spinner
-            :animation-duration="3000"
-            :size="60"
-            color="blue"
-          />
-        </client-only>
-      </v-row>
-    </v-container>
   </div>
 </template>
 
 <script>
-import { SpringSpinner } from 'epic-spinners'
 import HomePhoto from '../components/HomePhoto'
 import DiscoverHistoryLink from '../components/DiscoverHistoryLink'
 
@@ -44,36 +17,13 @@ export default {
   name: 'indexPage',
   components: {
     HomePhoto,
-    DiscoverHistoryLink,
-    SpringSpinner
+    DiscoverHistoryLink
   },
   data () {
     return {
-      loading: true
     }
-  },
-  beforeCreate () {
-    setTimeout(() => { this.loading = false }, 2 * 1000)
-    this.$router.beforeEach((to, from, next) => {
-      this.loading = true
-      next()
-    })
-    this.$router.afterEach(() => {
-      setTimeout(() => { this.loading = false }, 0.5 * 1000)
-    })
   }
 }
 </script>
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition-duration: 1s;
-  transition-property: opacity;
-  transition-timing-function: ease;
-}
-
-.fade-enter,
-.fade-leave-active {
-  opacity: 0
-}
 </style>
